6b19adf2f7f62643d32348c32455b3b8282ad9ee
Joel Chan <chozen86@gmail.com>
2024-12-19 08:14:00 -0500

    mvp viewer of 26 merged PECO examples

--- script.py
+++ script.py
@@ -3,7 +3,6 @@ from urllib.parse import quote # for double encoding IRIs for the API calls
 import time
 import json
 from docx import Document
-from jinja2 import Template
 
 def retrieve_ontology_matches(term, numresults=50, ontology=None):
     """
@@ -127,7 +126,7 @@ def rank_ontology_matches_by_distance(ontology_matches):
 
         # pause for a bit
         # so we don't get rate limited
-        time.sleep(1)
+        time.sleep(DELAY)
 
     # sort by distance from root
     matches_with_metadata.sort(key=lambda x: x['distance_from_root'])
@@ -154,7 +153,7 @@ def extract_peco_highlights_from_tables(doc_path):
         'TURQUOISE (3)': 'Comparator',
         'YELLOW (7)': 'Outcome'
     }
-
+    print(f"Found {len(doc.tables)} tables in the document")
     for table in doc.tables:
         table_dict = {}
         for row in table.rows:
@@ -178,6 +177,10 @@ def extract_peco_highlights_from_tables(doc_path):
             if "Section PECO statement is in" in row.cells[0].text.strip():
                 table_dict["doc_peco_section"] = row.cells[-1].text.strip()
 
+            # annotation note
+            if "Annotator comments" in row.cells[0].text.strip():
+                table_dict["doc_annotator_comments"] = row.cells[-1].text.strip()
+
             # PECO statement
             if "PECO statement" in row.cells[0].text.strip():  # Check for PECO row
                 peco_cell = row.cells[-1]  # Assume PECO text is in the 2nd column
@@ -190,6 +193,7 @@ def extract_peco_highlights_from_tables(doc_path):
                         text = run.text.strip()
                         if color and text:  # Only include highlighted text
                             color_key = str(color)
+                            # print(f"Found highlighted text: {text} with color {color_key}")
                             if color_key in annotation_map:
                                 color_key = annotation_map[color_key]
                                 if color_key not in color_text_map:
@@ -205,19 +209,22 @@ def extract_peco_highlights_from_tables(doc_path):
 
     return tables_data
 
-NUMRESULTS = 2
+NUMRESULTS = 5
+DELAY = 0.05
 
-doc_path = "data/PECO-examples_merged-example.docx"  
+doc_path = "data/PECO-examples_merged-26.docx"  
 
 # Extract PECO highlights from tables in the document
 annotations = extract_peco_highlights_from_tables(doc_path)
-print(annotations)
+print(f"Extracted {len(annotations)} PECO statement highlights in the document")
 
 parsed_output = []
 
 # for each highlight, retrieve ontology matches
 # TODO: handle keyphrases
 for annotation in annotations:
+    print(f"Retrieving ontology matches for {annotation['doc_title']}")
+    print(f"PECO elements: {annotation['peco_elements']}")
     annotation_d = {
         "doc_title": annotation["doc_title"],
         "doc_lastauthorname": annotation["doc_lastauthorname"],
@@ -237,13 +244,15 @@ for annotation in annotations:
             # get ontology matches
             jsonresults, ontology_matches = retrieve_ontology_matches(text, numresults=NUMRESULTS)
             # get ancestors and rank by distance from root
-            ranked_matches = rank_ontology_matches_by_distance(ontology_matches)
+            # removing this for now just to speed up the process
+            # ranked_matches = rank_ontology_matches_by_distance(ontology_matches)
 
             # get current list of elements for peco element
             current_elements = annotation_d["peco_elements"][peco_element]
             current_elements.append({
                 "text": text,
-                "matches": ranked_matches
+                # "matches": ranked_matches
+                "matches": ontology_matches
             })
     parsed_output.append(annotation_d)
 
@@ -258,35 +267,30 @@ with open("data/parsed_output.json", "w") as f:
 #         print(f"  Color: {color}, Text: {', '.join(texts)}")
 #     print("-" * 50)
 
+# html = """
 
-# Define the data to be displayed in the table
-data = [
-    {"Name": "John", "Age": 25, "City": "New York"},
-    {"Name": "Jane", "Age": 30, "City": "Los Angeles"},
-    {"Name": "Bob", "Age": 35, "City": "Chicago"}
-]
+# """
+# # Define the HTML table structure
+# html_table = """
+# <table border="1">
+#   <tr>
+#     <th>Preferred Term</th>
+#     <th>Ontology</th>
+#   </tr>
+# """
 
-# Define the HTML table template
-template = """
-<table border="1">
-  <tr>
-    {% for header in headers %}
-      <th>{{ header }}</th>
-    {% endfor %}
-  </tr>
-  {% for row in data %}
-    <tr>
-      {% for cell in row %}
-        <td>{{ cell }}</td>
-      {% endfor %}
-    </tr>
-  {% endfor %}
-</table>
-"""
+# # Loop through the data and add each row to the HTML table
+# for row in parsed_output:
+#     html_table += """
+#   <tr>
+#     <td>{Name}</td>
+#     <td>{Age}</td>
+#     <td>{City}</td>
+#   </tr>
+# """.format(**row)
 
-# Render the template with the data
-headers = ["Name", "Age", "City"]
-html_table = Template(template).render(headers=headers, data=data)
+# # Close the table tag
+# html_table += "</table>"
 
-# Print the HTML table to the console
-print(html_table)
+# # Print the HTML table to the console
+# print(html_table)
